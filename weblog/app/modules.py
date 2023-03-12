aeb2d40187d81fdd7e3563ce4c7536eb7c35de1a
Ms.Mohammadi <fatemeh.mohamady1377@yahoo.com>
2023-03-12 21:44:53 +0330

    "write relationship"

--- weblog/app/modules.py
+++ weblog/app/modules.py
@@ -12,6 +12,9 @@ class User(Base):
     user_name = Column(String(20), unique=True)
     user_email = Column(String(50), unique=True)
     password = Column(String(20))
+    posts = relationship("Post", back_populates="user")
+    comments = relationship("Comment", back_populates="user")
+
 
 
 class Post(Base):
@@ -24,6 +27,8 @@ class Post(Base):
     post_content = Column(Text)
     post_date = Column(TIMESTAMP)
     post_modified = Column(TIMESTAMP)
+    user = relationship("User", back_populates="posts")
+    comments = relationship("Comment", back_populates="post")
 
 
 class Comment(Base):
@@ -32,8 +37,9 @@ class Comment(Base):
     post_id = Column(Integer, ForeignKey("post.post_id"))
     user_name = Column(String(20))
     user_id = Column(Integer, ForeignKey("users.user_id"))
-    user_email = Column(String(50))
-    comment_date = Column(String(20))
+    comment_date = Column(TIMESTAMP)
     comment_text = Column(Text)
+    user = relationship("User", back_populates="comments")
+    post = relationship("Post", back_populates="comments")
 
 
