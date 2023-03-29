8b42734b6285fe41a57abc98d8fd02c961807060
fatemeh mohammadi <100574787+mohammadi1377@users.noreply.github.com>
2023-03-29 10:55:13 +0330

    Update user.py

--- weblog/app/router/user.py
+++ weblog/app/router/user.py
@@ -16,26 +16,6 @@ router = APIRouter(
 security = HTTPBearer()
 
 
-@router.get("/", response_model=List[UserSchemaOut])
-async def get_users(db: Session = Depends(get_db),
-                    current_user: User = Depends(oauth2.get_current_user)):
-    if current_user.role == 'admin':
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
-    users = db.query(User).all()
-    return users
-
-
-@router.get("/{id}", response_model=UserSchemaOut)
-async def get_user(id: int, db: Session = Depends(get_db),
-                   current_user: User = Depends(oauth2.get_current_user)):
-    if current_user.role == 'admin':
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
-    user = db.query(User).filter(User.id == id).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"User with id {id} not found.")
-    return user
-
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=UserSchemaOut)
 def create_user(user: CreateUserSchema, db: Session = Depends(get_db)):
     new_user = User(name=user.name,
@@ -62,16 +42,22 @@ async def update(user_id: int, user: CreateUserSchema, db: Session = Depends(get
 
 @router.delete('/user/{id}', status_code=200)
 async def delete_user(user_id: int, db: Session = Depends(get_db), current_user: User = Depends(oauth2.get_current_user)):
+
     if current_user.role not in ['admin', 'superuser']:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
+
     db_delete = db.query(User).get(user_id)
-    if db_delete.user_id != current_user.id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
+
     if not db_delete:
-        raise HTTPException(status_code=404, detail=f"Post {user_id} does not exist")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id:{user_id} was not found")
+
+    # if db_delete.id != current_user.id:
+    #     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
+
     db.delete(db_delete)
     db.commit()
-    return None
+    return {"detail": "user deleted successfully"}
 
 
 def check_if_exists(user, user_id):
