db4152b79718cfe69ba52448b9ffeb806a6d97ae
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-03-30 17:29:34 +0330

    now root.html runs when connecting to localhost

--- weblog/app/router/root.py
+++ weblog/app/router/root.py
@@ -1,10 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from .auth import template
 
 router = APIRouter(
     tags=["Root"]
 )
 
 
-@router.get("/")
-def root():
-    return {"message": "Welcome to  FastAPI WebBlog!"}
+@router.get("/", response_class=HTMLResponse)
+def root(request: Request):
+    return template.TemplateResponse(
+        "root.html",
+        {"request": request},
+    )
