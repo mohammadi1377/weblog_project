12294a126f5347ae3c21f437539e6526541c478b
fatemeh mohammadi <100574787+mohammadi1377@users.noreply.github.com>
2023-03-30 18:32:39 +0330

    Update main.py

--- weblog/main.py
+++ weblog/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.router import post, root, comment, user, auth,contact
+from app.router import post, root, comment, user, auth, contact, admin, home
 from app.DataBase.my_database import Base, engine
 
 
@@ -41,3 +41,5 @@ app.include_router(comment.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(contact.router)
+app.include_router(admin.router)
+app.include_router(home.router)
